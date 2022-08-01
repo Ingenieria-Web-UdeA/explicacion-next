edebf2dd20e71db52b2e6a8a857a9e52f1284626
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2022-08-01 13:50:29 +0100

    agregar explicacion next

---
+++ pages/daniel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Daniel = () => {
+  return (
+    <div>
+      <Head>
+        <title>Esta es la pagina 2</title>
+      </Head>
+      Esto es una nueva pagina
+    </div>
+  );
+};
+
+export default Daniel;
